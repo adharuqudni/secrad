acfc60bd8e03a0cc15329c3fe947401ea0b01d4f
adharuqudni <annas.adharuqudni@tiket.com>
2022-07-01 00:32:26 +0700

    add env checker

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import serviceAccount from './secret_key.js';
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(serviceAccount)
 }
