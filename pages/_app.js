8eb37fb0771bce46fbf7dbb15c86b7c15c0ecb17
adharuqudni <annas.adharuqudni@tiket.com>
2022-07-01 01:04:07 +0700

    change hello

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
-import admin from 'firebase-admin';
-if (!admin.apps.length) {
-  admin.initializeApp({});
+import * as firebase from "firebase-admin";
+if (!firebase.apps.length) {
+  firebase.initializeApp();
 }
 
 function MyApp({ Component, pageProps }) {
