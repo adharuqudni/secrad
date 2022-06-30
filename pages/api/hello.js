9a2c69bfddaf4438fbc8ec5c72ece9b319f153ce
adharuqudni <annas.adharuqudni@tiket.com>
2022-07-01 00:33:46 +0700

    change path

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import serviceAccount from './secret_key.js';
+import serviceAccount from '../../utils/db/secret_key';
 
 export default function handler(req, res) {
   res.status(200).json(serviceAccount)
