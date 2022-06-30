fa0e17b775a6e02207ace25315c601aa21741bc7
adharuqudni <annas.adharuqudni@tiket.com>
2022-07-01 01:27:58 +0700

    change private key

--- utils/db/index.js
+++ utils/db/index.js
@@ -2,10 +2,11 @@ import admin from 'firebase-admin';
 import serviceAccount from './secret_key.js';
 
 console.log(admin.apps);
+const { privateKey } = JSON.parse(serviceAccount.private_key);
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount)
+      credential: admin.credential.cert({...serviceAccount, privateKey: privateKey})
     });
   } catch (error) {
     console.log('Firebase admin initialization error', error.stack);
