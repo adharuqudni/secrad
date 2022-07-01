ccc2d57b20e4fc415057e8c79309cbaba25b3632
adharuqudni <annas.adharuqudni@tiket.com>
2022-07-02 00:38:43 +0700

    change env for apikey

--- utils/auth/firebase.js
+++ utils/auth/firebase.js
@@ -8,13 +8,12 @@ const { NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY, NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN, N
 const { apiKey } = JSON.parse(NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY);
 
 const FirebaseCredentials = {
-  apiKey: apiKey,
   authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID
 }
 // if a Firebase instance doesn't exist, create one
 if (!firebase?.apps.length) {
-  firebase.initializeApp(FirebaseCredentials)
+  firebase.initializeApp({...FirebaseCredentials, apiKey: apiKey})
 }
 
 export default firebase;
